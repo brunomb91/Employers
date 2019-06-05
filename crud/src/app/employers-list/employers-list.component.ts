d47a69c5d2ec8b39a7331b23d100dc6378ad8cdb
unknown <bruno.mb.91@gmail.com>
2019-06-05 13:03:17 -0300

    Completando interface

--- crud/src/app/employers-list/employers-list.component.ts
+++ crud/src/app/employers-list/employers-list.component.ts
@@ -1,15 +1,72 @@
 import { Component, OnInit } from '@angular/core';
+import { EmployersService } from '../employers.service';
 
 @Component({
   selector: 'app-employers-list',
   templateUrl: './employers-list.component.html',
-  styleUrls: ['./employers-list.component.css']
+  styleUrls: ['./employers-list.component.css'],
+  providers: [EmployersService]
 })
-export class EmployersListComponent implements OnInit {
+export class EmployersListComponent {
+  employers = [{name:'Bruno'},{name:'Bruno2'}];
+  selectedEmployer;
+  postActivated = true;
+  divActivated = false;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private api:EmployersService) {
+    this.getEmployers();
+    this.selectedEmployer = {id: -1, name: '', address: '', salary: 0.0, badgeNumber: 0, dateJoined: ''};
   }
+
+  getEmployers = () => {
+    this.api.getAllEmployers().subscribe(
+      data => {
+        this.employers = data;
+      },
+      error => {
+        console.log(error);
+      })
+    }
+
+  employerClicked = (employer) => {
+        console.log(employer.id);
+        this.api.getOneEmployer(employer.id).subscribe(
+          data => {
+            this.selectedEmployer = data;
+          },
+          error => {
+            console.log(error);
+          })
+    }
+
+    updateEmployer = () => {
+      this.api.updateEmployer(this.selectedEmployer).subscribe(
+        data => {
+          this.getEmployers();
+        },
+        error => {
+          console.log(error);
+        })
+      }
+
+    createEmployer = () => {
+      this.api.createEmployer(this.selectedEmployer).subscribe(
+        data => {
+          this.employers.push(data);
+        },
+        error => {
+          console.log(error);
+        })
+    }
+
+    deleteEmployer = () => {
+        this.api.deleteEmployer(this.selectedEmployer.id).subscribe(
+          data => {
+            this.getEmployers();
+          },
+          error => {
+            console.log(error);
+          })
+    }
 
 }
--- crud/src/app/employers.service.ts
+++ crud/src/app/employers.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class EmployersService {
 
-  constructor() { }
+export class EmployersService {
+  baseurl = "http://127.0.0.1:8000";
+  httpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
+
+  constructor(private http: HttpClient) { }
+
+  getAllEmployers(): Observable<any> {
+    return this.http.get(this.baseurl + '/employers/',
+    {headers: this.httpHeaders});
+  }
+
+  getOneEmployer(id): Observable<any> {
+    return this.http.get(this.baseurl + '/employers/' + id + '/',
+    {headers: this.httpHeaders});
+  }
+
+  updateEmployer(employer): Observable<any> {
+    const body =  {name: employer.name, address: employer.address, salary: employer.salary, badgeNumber: employer.badgeNumber, dateJoined: employer.dateJoined};
+    return this.http.put(this.baseurl + '/employers/' + employer.id + '/', body,
+    {headers: this.httpHeaders});
+  }
+
+  createEmployer(employer): Observable<any> {
+    const body =  {name: employer.name, address: employer.address, salary: employer.salary, badgeNumber: employer.badgeNumber, dateJoined: employer.dateJoined};
+    return this.http.post(this.baseurl + '/employers/', body,
+    {headers: this.httpHeaders});
+  }
+
+  deleteEmployer(id): Observable<any> {
+    return this.http.delete(this.baseurl + '/employers/' + id + '/',
+    {headers: this.httpHeaders});
+  }
+
 }
